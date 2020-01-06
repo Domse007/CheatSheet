b8fe9cca179b86618380fbc36896dcf125bc7210
Domse007 <dominikkeller55@gmail.com>
2020-01-06 22:10:24 +0100

    maybe fixed all merge mistakes

--- scripts/onetimepads.js
+++ scripts/onetimepads.js
@@ -1,8 +1,10 @@
+var str_A; 
+
 function add_strings() {
 
     alphabeth = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    var str_A = document.getElementById("inputOTP1").value;
+    str_A = document.getElementById("inputOTP1").value;
     var str_B = document.getElementById("inputOTP2").value;
 
     str_A = str_A.toUpperCase();
@@ -15,6 +17,13 @@ function add_strings() {
         result += alphabeth.charAt(new_letter_pos);
     };
 
-    document.getElementById('smalllogOTP1').innerHTML = `<div class="black-text"; style="margin-left: 5px;"> Das Resultat lautet: ` + result + `</div>`;
+    document.getElementById('smalllogOTP1').innerHTML = `<div class="black-text" style="margin-left: 5px;"> Das Resultat lautet: ` + result + `</div><div class="black-text" id="1">` + str_A + `</div><div id="second">` + str_B + `</div>`;
+
+};
+function sub_strings() {
+
+    var str_A = document.getElementById("1").value;
+
+    document.getElementById('smalllogOTP2').innerHTML = `<div class="black-text"; style="margin-left: 5px;"> Das Resultat lautet: ` + str_A + `</div>`;
 
 };
