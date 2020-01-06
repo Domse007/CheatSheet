bf5c0821ccb90b7b2594ce5855948e2bfb0d3fc7
Domse007 <dominikkeller55@gmail.com>
2020-01-06 22:21:07 +0100

    maybe added all things; Merge fucked everything up

--- scripts/onetimepads.js
+++ scripts/onetimepads.js
@@ -20,10 +20,3 @@ function add_strings() {
     document.getElementById('smalllogOTP1').innerHTML = `<div class="black-text" style="margin-left: 5px;"> Das Resultat lautet: ` + result + `</div><div class="black-text" id="1">` + str_A + `</div><div id="second">` + str_B + `</div>`;
 
 };
-function sub_strings() {
-
-    var str_A = document.getElementById("1").value;
-
-    document.getElementById('smalllogOTP2').innerHTML = `<div class="black-text"; style="margin-left: 5px;"> Das Resultat lautet: ` + str_A + `</div>`;
-
-};
