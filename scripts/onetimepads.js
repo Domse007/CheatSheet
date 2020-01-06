c10a5962cf7e61f2277c4a8ebb8c7e83043fc84c
Domse007 <dominikkeller55@gmail.com>
2020-01-06 20:55:30 +0100

    newest changes

--- scripts/onetimepads.js
+++ scripts/onetimepads.js
@@ -15,6 +15,25 @@ function add_strings(){
         result = result + alphabet.charAt(new_letter_pos);
     };
 
-    document.getElementById('smalllogOTP1').innerHTML = `<div class="black-text"; style="margin-left: 5px;"> Das Resultat lautet: ` + result + `</div>`;
+    document.getElementById('smalllogOTP1').innerHTML = `<div class="black-text"; style="margin-left: 5px;"> Das Resultat lautet: ` + result + `</div><div id="first">` + str_A + `</div><div id="second">` + str_B + `</div>`;
+};
 
+function sub_strings(){
+
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    var str_A = document.getElementById("second").value;
+    var str_B = document.getElementById("first").value;
+
+    str_A = str_A.toUpperCase();
+    str_B = str_B.toUpperCase();
+
+    var result = "";
+
+    for(var i = 0; i > str_A.length; i++){
+        new_letter_pos=(alphabet.search(str_A.charAt(i)) + alphabet.search(str_B.charAt(i))) % 26;
+        result = result + alphabet.charAt(new_letter_pos);
+    };
+
+    document.getElementById('smalllogOTP1').innerHTML = `<div class="black-text"; style="margin-left: 5px;"> Das Resultat lautet: ` + result + `</div>`;
 };
